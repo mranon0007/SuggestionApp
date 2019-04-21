8afea25003bbaf2200e78a052a05abb7173aee09
mranon0007 <mranon0007@gmail.com>
2019-04-21 22:18:20 +0500

    Basic Functionality added of SuggestionApp

--- SuggestionApp.py
+++ SuggestionApp.py
@@ -2,15 +2,32 @@ from database import Db
 
 class SuggestionApp:
     
+    def __init__(self):
+        self.db = Db()
+
     # Add the word to database, skip if it already exsists
     def addToDb(self, word, skip_if_exsists=True):
-        words = tokenizeString(word)
+        words = self.tokenizeString(word)
+        self.db.addList(words)
         return
 
     # tokenize the string
+    # split on whitespaces. 
+    # Also return the complete sentence except the last word as it might be incomplete
+    # filter empty strings
     def tokenizeString(self, string):
-        return
+        splitString = string.split()
+        if(len(splitString) < 2): return []
+        return filter(None, splitString + [" ".join(splitString[:-1])])
 
     # search for the word in db and return Suggestions
+    # Add new words to DB
     def getSuggestions(self, query):
-        return
+        # Add new words to DB
+        self.addToDb(query)
+        suggestions = self.db.getWordsByQuery(query)
+        # get words from DB
+        return suggestions
+
+app = SuggestionApp()
+print(app.getSuggestions("cri"))

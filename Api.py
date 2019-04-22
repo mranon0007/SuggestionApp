b990c95087f2fa08d4e9fe5ec83c3e58c9900f2d
mranon0007 <mranon0007@gmail.com>
2019-04-22 11:34:17 +0500

    Setup WebSocket Server

--- Api.py
+++ Api.py
@@ -1,1 +1,20 @@
-import websocket-server
+from websocket_server import WebsocketServer
+import logging
+import json
+
+from SuggestionApp import SuggestionApp
+sApp = SuggestionApp()
+
+def client_msg_rcvd(client, server, message):
+    message = json.loads(message)
+    if message['type'] == "autocomplete":
+        res = sApp.getSuggestions(message['data']['query'], lang=message['data']['lang'], limit=message['data']['limit'])
+        res = json.dumps(res)
+        server.send_message(client, "echo")
+    else:
+        raise Exception("Incorrect Msg type received")
+    return
+
+server = WebsocketServer(13254, host='127.0.0.1')
+server.set_fn_message_received(client_msg_rcvd)
+server.run_forever()
